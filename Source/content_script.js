9176fc0243d3b806b21b13dccf0b9e798254e954
connorgutman <connorgutman@gmail.com>
2017-03-07 14:40:06 -0500

    personalized

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,47 +1,44 @@
 walk(document.body);
 
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
-	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
-		return;
-	}
+function walk(node) {
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+  var child, next;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+  if (node.nodeName.toLowerCase() == 'input' || node.nodeName.toLowerCase() == 'textarea' || (node.classList && node.classList.contains('ace_editor'))) {
+    return;
+  }
+  switch (node.nodeType) {
+    case 1: // Element
+    case 9: // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
+function handleText(textNode) {
+  var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+  v = v.replace(/\bCIA\b/g, "INGSOC");
+  v = v.replace(/\bCia\b/g, "Ingsoc");
+  v = v.replace(/\bcia\b/g, "ingsoc");
+	v = v.replace(/\bc.i.a\b/g, "ingsoc");
+	v = v.replace(/\bC.I.A\b/g, "INGSOC");
+	v = v.replace(/\bc.i.a.\b/g, "ingsoc");
+	v = v.replace(/\bC.I.A.\b/g, "INGSOC");
+	v = v.replace(/\bthe C.I.A.\b/g, "INGSOC");
+	v = v.replace(/\bThe C.I.A.\b/g, "INGSOC");
+
+  textNode.nodeValue = v;
 }
-
-
